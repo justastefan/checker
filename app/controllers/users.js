ca34cb34a10b69ab423f18e7397b2ab070c55c1b
justastefan <stefanmi@hotmail.com>
2017-11-03 23:13:53 +0100

    delete user (except yourself), fixed DELETE empty response

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 export default Controller.extend({
   store: service(),
+  session: service(),
   actions: {
     addUser() {
       let newUser = this.get('store').createRecord('user', {
@@ -9,6 +10,17 @@ export default Controller.extend({
         email: this.get('model.email')
       });
       newUser.save().then(()=>{
+        // users route refreshen
+        this.send('refreshRoute');
+      });
+    },
+    deleteUser(user) {
+      if (this.get('session.user.id') === user.get('id')) {
+        return;
+      }
+
+      user.deleteRecord();
+      user.save().then(()=>{
         // users route refreshen
         this.send('refreshRoute');
       });
